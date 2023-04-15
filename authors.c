359e6be1c2205260c927307abd093b04c0344d71
Esianyo <esianicd@gmail.com>
2023-04-15 11:41:05 -0700

    A beautiful code for betty

---
+++ authors.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 for success
+ */
+
+int main(void)
+{
+	char *auth1 = "Dr. Leena Kanandjebo";
+	char *auth2 = "Esianyo Dzisenu";
+
+	printf("Authors of this are: \n");
+	printf("%s and %s\n", auth1, auth2);
+
+	return (0);
+}
