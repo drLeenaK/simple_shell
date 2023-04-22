1dd2791ee9717f6e3be89ab8e9feefeb0d93f87b
Esianyo <esianicd@gmail.com>
2023-04-22 12:37:43 -0700

    updating builtins.c file

--- builtins.c
+++ builtins.c
@@ -4,38 +4,73 @@
 #include <stdio.h>
 
 /**
+ * token_interface - tokenize a string
+ * @line: string to be tokenized
+ * @delim: delimiter used for tokenization
+ * @token_count: pointer to the number of tokens
+ *
+ * Return: array of tokens
+ */
+
+char **token_interface(char *line, const char *delim, int *token_count)
+{
+        char **tokens;
+        char *token;
+        int i;
+
+        tokens = malloc(sizeof(char *) * 1024);
+        if (tokens == NULL)
+        {
+                perror("malloc failed");
+                exit(EXIT_FAILURE);
+        }
+
+        token = strtok(line, delim);
+        for (i = 0; token != NULL; i++)
+        {
+                tokens[i] = token;
+                token = strtok(NULL, delim);
+        }
+        tokens[i] = NULL;
+        *token_count = i;
+
+        return (tokens);
+}
+
+/**
  * cd_b - changes the current working directory
- * 	if no parameter is passed it will change directory to HOME.
+ *      if no parameter is passed it will change directory to HOME.
  * @line: string representing the input
  */
 
 void cd_b(char *line)
 {
-	int index;
-	int token_count;
-	char **param_array;
-	const char *delim = "\n\t ";
-	char **token_interface(char *line, const char *delim, int token_count);
+        int index;
+        int token_count;
+        char **param_array;
+        const char *delim = "\n\t ";
+        char **token_interface(char *line, const char *delim, int *token_count);
 
-	token_count = 0;
-	param_array = token_interface(line, delim, token_count);
+        token_count = 0;
+        param_array = token_interface(line, delim, &token_count);
 
-	if (param_array[0] == NULL)
-	{
-		single_free(2, param_array, line);
-		return;
-	}
-	if (param_array[1] == NULL)
-	{
-		index = find_path("HOME");
-		chdir((environ[index]) + 5);
-	}
-	else if (_strcmp(param_array[1], "-") == 0)
-		print_str(param_array[1], 0);
+        if (param_array[0] == NULL)
+        {
+                double_free(param_array);
+                return;
+        }
 
-	else
-		chdir(param_array[1]);
-	double_free(param_array);
+        if (param_array[1] == NULL)
+        {
+                index = find_path("HOME");
+                chdir((environ[index]) + 5);
+        }
+        else if (_strcmp(param_array[1], "-") == 0)
+                printf("%s\n", param_array[1]);
+        else
+                chdir(param_array[1]);
+
+        double_free(param_array);
 }
 
 /**
@@ -47,15 +82,12 @@ void cd_b(char *line)
 
 void env_b(__attribute__((unused))char *line)
 {
-	int i;
-	int j;
+        int i;
 
-	for (i = 0; environ[i] != NULL; i++)
-	{
-		for (j = 0; environ[i][j] != '\0'; j++)
-			write(STDOUT_FILENO, &environ[i][j], 1);
-		write(STDOUT_FILENO, "\n", 1);
-	}
+        for (i = 0; environ[i] != NULL; i++)
+        {
+                printf("%s\n", environ[i]);
+        }
 }
 
 /**
@@ -64,11 +96,11 @@ void env_b(__attribute__((unused))char *line)
  *
  * Return: nothing.
  */
+
 void exit_b(char *line)
 {
-	free(line);
-	print_str("\n", 1);
-	exit(1);
+        free(line);
+        exit(0);
 }
 
 /**
@@ -77,25 +109,26 @@ void exit_b(char *line)
  *
  * Return: NULL if failed
  */
+
 void (*check_built_ins(char *str))(char *str)
 {
-	int i;
+        int i;
 
-	builtin_t buildin[] = {
-		{"exit", exit_b},
-		{"env", env_b},
-		{"cd", cd_b},
-		{NULL, NULL}
-	};
+        builtin_t buildin[] = {
+                {"exit", exit_b},
+                {"env", env_b},
+                {"cd", cd_b},
+                {NULL, NULL}
+        };
 
-	for (i = 0; buildin[i].built != NULL; i++)
-	{
-		if (_strcmp(str, buildin[i].built) == 0)
-		{
-			return (buildin[i].f);
-		}
-	}
-	return (NULL);
+        for (i = 0; buildin[i].built != NULL; i++)
+        {
+                if (_strcmp(str, buildin[i].built) == 0)
+                {
+                        return (buildin[i].f);
+                }
+        }
+        return (NULL);
 }
 
 /**
@@ -105,15 +138,16 @@ void (*check_built_ins(char *str))(char *str)
  *
  * Return: 0 for success, -1 for failure
  */
+
 int built_in(char **command, char *line)
 {
-	void (*build)(char *);
+        void (*build)(char *);
 
-	build = check_built_ins(command[0]);
-	if (build == NULL)
-		return (-1);
-	if (_strcmp("exit", command[0]) == 0)
-		double_free(command);
-	build(line);
-	return (0);
+        build = check_built_ins(command[0]);
+        if (build == NULL)
+                return (-1);
+        if (_strcmp("exit", command[0]) == 0)
+                double_free(command);
+        build(line);
+        return (0);
 }
